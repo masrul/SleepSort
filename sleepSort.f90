c0515cee4212d04d5cbee0f7aaac926afd2fa83e
masrul <mail2masrul@gmail.com>
2019-08-15 22:28:54 -0500

    fix indentation

--- sleepSort.f90
+++ sleepSort.f90
@@ -1,19 +1,20 @@
 program sleepSort
-	use omp_lib
-    implicit none
-    integer::nArgs,myid,i,stat
-    integer,allocatable::intArg(:)
-    character(len=5)::arg
+   use omp_lib
+   implicit none
+   integer::nArgs,myid,i,stat
+   integer,allocatable::intArg(:)
+   character(len=5)::arg
 
-    !$omp master
-    nArgs=command_argument_count()
-    if(nArgs==0)stop ' : No argument is given !'
+   !$omp master
+   nArgs=command_argument_count()
+   if(nArgs==0)stop ' : No argument is given !'
 	allocate(intArg(nArgs))
 	do i=1,nArgs
 		call get_command_argument(i, arg)
 		read(arg,'(I5)',iostat=stat)intArg(i)
-		if(intArg(i)<0 .or. stat/=0) stop&
-        &' :Only 0 or positive integer allowed !'
+		if(intArg(i)<0 .or. stat/=0) then 
+         stop ' Only 0 or positive integer allowed !'
+      endif 
 	end do
 	call omp_set_num_threads(nArgs)
 	!$omp end master
