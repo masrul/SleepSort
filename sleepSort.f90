752750df8b737a19334460519348cc8eeb161bbb
masrul <mail2masrul@gmail.com>
2019-08-15 22:31:04 -0500

    fix indentation

--- sleepSort.f90
+++ sleepSort.f90
@@ -7,30 +7,31 @@ program sleepSort
 
    !$omp master
    nArgs=command_argument_count()
-   if(nArgs==0)stop ' : No argument is given !'
-	allocate(intArg(nArgs))
-	do i=1,nArgs
-		call get_command_argument(i, arg)
-		read(arg,'(I5)',iostat=stat)intArg(i)
-		if(intArg(i)<0 .or. stat/=0) then 
+   if(nArgs==0)stop ' No argument is given !'
+   
+   allocate(intArg(nArgs))
+   do i=1,nArgs
+      call get_command_argument(i, arg)
+      read(arg,'(I5)',iostat=stat)intArg(i)
+      if(intArg(i)<0 .or. stat/=0) then 
          stop ' Only 0 or positive integer allowed !'
       endif 
-	end do
-	call omp_set_num_threads(nArgs)
-	!$omp end master
- 
+   end do
+   call omp_set_num_threads(nArgs)
+   !$omp end master
 
-	!$omp parallel private(myid)
-	myid =omp_get_thread_num()
-	call sleepNprint(intArg(myid+1))
-	!$omp end parallel
 
-contains
-	subroutine sleepNprint(nSeconds)
-		integer::nSeconds
-		call sleep(nSeconds)
-		print*,nSeconds
-	end subroutine sleepNprint
+   !$omp parallel private(myid)
+   myid =omp_get_thread_num()
+   call sleepNprint(intArg(myid+1))
+   !$omp end parallel
+
+   contains
+   subroutine sleepNprint(nSeconds)
+      integer::nSeconds
+      call sleep(nSeconds)
+      print*,nSeconds
+   end subroutine sleepNprint
 end program sleepSort
 
 
